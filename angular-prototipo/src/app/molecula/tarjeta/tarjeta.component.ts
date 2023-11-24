e228c6bb7f9a57a915a0bd218c3923150b751a1e
ivangonzalez02 <ivafelgonzalez@uniboyaca.edu.com>
2023-11-24 17:22:47 -0500

    interfaz primaria

--- angular-prototipo/src/app/molecula/tarjeta/tarjeta.component.ts
+++ angular-prototipo/src/app/molecula/tarjeta/tarjeta.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TituloComponent } from "./titulo/titulo.component";
+import { BotonComponent } from "./boton/boton.component";
+import { ContenidoComponent } from "./contenido/contenido.component";
 
 @Component({
-  selector: 'app-tarjeta',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tarjeta.component.html',
-  styleUrl: './tarjeta.component.scss'
+    selector: 'app-tarjeta',
+    standalone: true,
+    templateUrl: './tarjeta.component.html',
+    styleUrl: './tarjeta.component.scss',
+    imports: [CommonModule, TituloComponent, BotonComponent, ContenidoComponent]
 })
 export class TarjetaComponent {
 
